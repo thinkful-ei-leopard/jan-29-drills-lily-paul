f274f43bca2dbd6062c2503c33e5344a08147344
wavinginspace <paulbaisley@gmail.com>
2020-01-29 11:21:21 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TheDate from './state/TheDate.js';
+import Counter from './state/Counter.js';
+import Tabs from './state/Tabs.js';
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+function App() {
+  return (
+    <main className="App">
+      {/* <TheDate /> */}
+      {/* <Counter count={123} number={5} /> */}
+      <Tabs tabs={tabsProp} />
+    </main>
+  );
+}
+
+export default App;
