b6dc09fb393a08d9b2c47bfbb763b78c0c1ab02a
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-29 10:25:59 -0600

    made hello world

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import HelloWorld from './state-drills/HelloWorld';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,3 @@
+class HelloWorld extends React.Component {
+  
+}
